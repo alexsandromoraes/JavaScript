40d31c07c3a4e2b2c6564f72a627befbe959de69
alexsandromoraes <alexsandromoraes101@gmail.com>
2026-02-05 18:31:18 -0300

    aula 11

--- Aulas/aula11/Ex001/index.js
+++ Aulas/aula11/Ex001/index.js
@@ -1,1 +1,6 @@
-console.log('O console esta funcionando!')
+var vel = 50.
+console.log(`A velocidade do seu carro é ${vel}ka/h`)
+if (vel > 60) {// Condição simples pos não tem ELSE
+    console.log(`A velocidade foi exedida! MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
