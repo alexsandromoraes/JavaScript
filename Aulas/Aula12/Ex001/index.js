08521fa0f695546724135698b320176a1cc7d6bd
alexsandromoraes <alexsandromoraes101@gmail.com>
2026-02-09 17:02:02 -0300

    inicio da aula 12

---
+++ Aulas/Aula12/Ex001/index.js
@@ -0,0 +1,10 @@
+var idade = 197
+if (idade < 16){
+    console.log("Não vota")
+} else {
+    if (idade >=16, idade < 18) {
+        console.log('Você pode votar')
+    } else {
+        console.log('Você vota')
+    }
+}
